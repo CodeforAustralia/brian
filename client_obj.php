8b23edbcef7b24fc349a9f921ad00ae804b2e0a3
Samara <samaradionne@gmail.com>
2017-10-23 16:22:39 +1100

    Community Work
    Changed Locations to CCSLocations, added CommunityWork to object

--- client_obj.php
+++ client_obj.php
@@ -1,30 +1,35 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
-// header("Access-Control-Allow-Origin: http://ec2-54-66-246-123.ap-southeast-2.compute.amazonaws.com:3000");
 header("Content-Type:application/json");
 
 require_once "db.php";
 require_once "pretty_json.php";
 
 $client_obj = '';
+$debug = '';
 
 $JAID = '';
 $FirstName = '';
 $LastName = '';
+$message_arr = array();
+$phone_arr = array();
+$ccs_location_arr = array();
+$cw_location_arr = array();
+$program_location_arr = array();
 
-$debug = '';
 
+// Check if Debugging mode is needed
 if(isset($_GET["debug"]) && !empty($_GET["debug"])) {
 	$debug = $_GET['debug'];
 }
 
+// 
 if(isset($_GET["JAID"]) && !empty($_GET["JAID"])) {
 	$JAID = $_GET['JAID'];
 
-	$phone_Arr = array();
-	$location_Arr = array();
 
+
+	// Get JAID, first name, last name
 	try {
 		$offender_sql = "SELECT * FROM testdb.Offender WHERE JAID = " . $JAID . ";";
 
@@ -38,12 +43,14 @@ if(isset($_GET["JAID"]) && !empty($_GET["JAID"])) {
 	    echo "Database Error";
 	}
 
+
+	// Get phone numbers in ascending order of EndDate
 	try {
-		$phone_sql = "SELECT * FROM testdb.Phone WHERE JAID = " . $JAID . ";";
+		$phone_sql = "SELECT * FROM testdb.Phone WHERE JAID = " . $JAID . " ORDER BY EndDate ASC;";
 
 		foreach(returnDB()->query($phone_sql) as $row) {
 
-			$phone_Arr[] = array(
+			$phone_arr[] = array(
 				'PhoneNumber' => $row['PhoneNumber'],
 				'StartDate' => $row['StartDate'],
 				'EndDate' => $row['EndDate'],
@@ -54,21 +61,24 @@ if(isset($_GET["JAID"]) && !empty($_GET["JAID"])) {
 	    echo "Database Error!";
 	}
 
+
+	// Get CCS locations in ascending order of EndDate
 	try {
-			$location_sql = "SELECT 
-		* 
-	FROM 
-		testdb.Offender o
-			JOIN testdb.OffenderCCSLocation c
-				ON o.JAID=c.JAID
-			JOIN testdb.Location l
-				ON c.LocationID=l.LocationID
-	WHERE 
-		o.JAID = '". $JAID . "';";
+			$ccs_location_sql = "
+			SELECT 
+				* 
+					FROM 
+						testdb.Offender o
+							JOIN testdb.OffenderCCSLocation c
+								ON o.JAID=c.JAID
+							JOIN testdb.Location l
+								ON c.LocationID=l.LocationID
+					WHERE 
+						o.JAID = '". $JAID . "' ORDER BY EndDate ASC;";
 
-		foreach(returnDB()->query($location_sql) as $row) {
+		foreach(returnDB()->query($ccs_location_sql) as $row) {
 
-			$location_Arr[] = array(
+			$ccs_location_arr[] = array(
 				'StartDate' => $row['StartDate'],
 				'EndDate' => $row['EndDate'],
 				'SiteName' => $row['SiteName'],
@@ -85,12 +95,55 @@ if(isset($_GET["JAID"]) && !empty($_GET["JAID"])) {
 	}
 
 
+	// Get Community Work in ascending order of EndDate
+	// TODO: Select distinct values (ProgramID & Location ID duplicated)
+	try {
+			$cw_location_sql = "
+				SELECT 
+				    *
+				FROM
+			    testdb.Appointment a
+			        JOIN
+			    testdb.Program p ON a.ProgramID = p.ProgramID
+			        JOIN
+			    testdb.Location l ON p.LocationID = l.LocationID
+			        JOIN
+			    testdb.Region r ON r.RegionID = l.RegionID
+				WHERE
+					a.JAID = '". $JAID . "'
+		        AND a.Type = 'CommunityWork' ORDER BY EndTime ASC;'";
+
+		foreach(returnDB()->query($cw_location_sql) as $row) {
+
+			$cw_location_arr[] = array(
+				'ProgramName' => $row['ProgramName'],
+				'StartTime' => $row['StartTime'],
+				'EndTime' => $row['EndTime'],
+				'ContactName' => $row['ContactName'],
+				'Notes' => $row['Notes'],
+				'Details' => $row['Details'],
+				'SiteName' => $row['SiteName'],
+				'GMapsAddress' => $row['GMapsAddress'],
+				'PlaintextAddress' => $row['PlaintextAddress'],
+				'PhoneNumber' => $row['PhoneNumber'],
+				'EmailAddress' => $row['EmailAddress'],
+				'OSMSNumber' => $row['OSMSNumber'],
+				'RegionName' => $row['RegionName'],
+			);
+		}
+
+	} catch (Exception $e) {
+	    echo "<p>Database Error!</p>";
+	}
+
+
+	// Get messages in ascending order of DateDelivered
 	try {
 		$message_sql = "SELECT * FROM testdb.Message WHERE JAID = " . $JAID . " ORDER BY DateDelivered ASC;";
 
 		foreach(returnDB()->query($message_sql) as $row) {
 
-			$message_Arr[] = array(
+			$message_arr[] = array(
 				'MessageType' => $row['MessageType'],
 				'Outbound' => $row['Outbound'],
 				'To' => $row['To'],
@@ -107,47 +160,29 @@ if(isset($_GET["JAID"]) && !empty($_GET["JAID"])) {
 	}
 
 
-
+	// Create JSON encoded object to be served with previous array data
+	// (phones, ccs locations, programs, messages)
 	$client_obj = array(
 		'JAID' => $JAID,
 		'FirstName' => $FirstName,
 		'LastName' => $LastName,
-		'Phones' => $phone_Arr,
-		'Locations' => $location_Arr,
-		'Messages' => $message_Arr
+		'Phones' => $phone_arr,
+		'CCSLocations' => $ccs_location_arr,
+		'CommunityWork' => $cw_location_arr,
+		'Messages' => $message_arr
 	);
-	
 	$json_client_obj = json_encode($client_obj);
 
+
+	// Debugging modes
 	if($debug) {
-		print "<pre>";
 		print_r(indent($json_client_obj));
-		print "</pre>";
 	}
 	else {
 		$json_client_obj = json_encode($client_obj);
 		echo $json_client_obj;
 	}
-	
-	// if(empty($price))	{
-	// 	response(200,"Product Not Found",NULL);
-	// }
-	// else {
-	// 	response(200,"Product Found",$json_client_obj);
-	// }
-
 }
 else {
 	// response(400,"Invalid Request",NULL);
 }
-
-// function response($status,$status_message,$data) {
-// 	header("HTTP/1.1 ".$status_message);
-	
-// 	$response['status']=$status;
-// 	$response['status_message']=$status_message;
-// 	$response['data']=$data;
-	
-// 	$json_response = json_encode($response);
-// 	echo $json_response;
-// }
--- showcase.php
+++ showcase.php
@@ -1,6 +1,10 @@
 <?php 
 
-require_once "add.php";
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Origin: http://ec2-54-66-246-123.ap-southeast-2.compute.amazonaws.com:3000");
+header("Content-Type:application/json");
+
+require_once "send_message.php";
 
 $id = 'IPSUM';
 $to = $_GET['to'];
