3c51b67a5dd5c6ff1354667c17814a701af748dc
Sam <blouish@hotmail.com>
2017-11-02 04:18:22 +0000

    Cleanup

--- db.php
+++
@@ -1,5 +0,0 @@
-<?php
-function returnDB() {
-	return new PDO('mysql:host=testdb.ccce7v12q5fq.ap-southeast-2.rds.amazonaws.com:3306;dbname=testdb', 'admin', 'd74AJjLnVYEHKLqyvK74yTsrZ4cx739iD3QnwMe4');
-}
-
