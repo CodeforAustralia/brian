f0058ad32929d246855c303e742a35c3ed4a226a
Sam <blouish@hotmail.com>
2017-11-09 01:45:14 +0000

    get_staff_from_location

--- src/staff_obj.php
+++ src/staff_obj.php
@@ -36,6 +36,39 @@ WHERE
 	return $user_arr;
 }
 
+function get_staff_from_location($id) {
+	try {
+		$user_sql = "SELECT 
+    *
+FROM
+    testdb.Staff s
+        JOIN
+    testdb.StaffLocation sl
+        JOIN
+    testdb.Location l
+WHERE
+    s.email = sl.email
+        AND sl.LocationID = l.LocationID
+        AND sl.LocationID = '" . $id "';";
+
+		foreach(returnDB()->query($user_sql) as $row) {
+
+			$user_arr[] = array(
+				'email' => $row['email'],
+				'FirstName' => $row['FirstName'],
+				'LastName' => $row['LastName'],
+				'LocationID' => $row['LocationID'],
+				'SiteName' => $row['SiteName'],
+			);
+		}
+
+	} catch (Exception $e) {
+	    echo "Database Error!";
+	}
+
+	return $user_arr;
+}
+
 // Create JSON encoded object to be served with previous array data
 // TODO: Condense all of these outputs
 function staff_output($arr) {
