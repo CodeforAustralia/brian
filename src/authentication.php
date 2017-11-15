74cf5c8874b2405cb51a330930265c084ab605b9
Sam <blouish@hotmail.com>
2017-11-15 23:09:27 +0000

    Removing all files in .gitignore

---
+++ src/authentication.php
@@ -0,0 +1,11 @@
+<?php
+
+function getPassword() {
+	$password = 'FnTEtnvLg!&z?Z4P$%FW@Wb#+U^z?F9S';
+	return $password;
+}
+
+function getUsername() {
+	$username = 'zKZHfCVA3yNqFxzs';
+	return $username;
+}
