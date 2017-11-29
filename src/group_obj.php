8a0932279689cb06dfad9c77633ba5b3b7048547
Sam <blouish@hotmail.com>
2017-11-29 11:49:07 +0000

    Group at location - archived

--- src/group_obj.php
+++ src/group_obj.php
@@ -223,6 +223,33 @@ function get_group_location($id) {
 	return $group_arr;
 }
 
+// Get specific group
+function get_group_location_archived($id) {
+
+	try {
+		$group_sql = "SELECT * FROM testdb.Group WHERE GroupLocation = '" . $id "' AND Archived IS NOT NULL;";
+
+		foreach(returnDB()->query($group_sql) as $row) {
+
+			$group_arr[] = array(
+				'GroupID' => $row['GroupID'],
+				'GroupName' => $row['GroupName'],
+				'GroupAuthor' => $row['GroupAuthor'],
+				'DateCreated' => $row['DateCreated'],
+				'LastUpdatedAuthor' => $row['LastUpdatedAuthor'],
+				'LastUpdated' => $row['LastUpdated'],
+				'Archived' => $row['Archived'],
+				'ArchiveDate' => $row['ArchiveDate'],
+				'Archivist' => $row['Archivist'],
+			);
+		}
+
+	} catch (Exception $e) {
+	    echo "Database Error!";
+	}
+
+	return $group_arr;
+}
 
 
 // Create JSON encoded object to be served with previous array data
